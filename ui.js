0bbc78677ac5f3f695143f72272e514b2a6aefeb
JavaRip <UP941226@myport.ac.uk>
2019-12-29 20:31:39 +0000

    camel positioner added

--- ui.js
+++ ui.js
@@ -1,13 +1,4 @@
-
-console.log(GameState);
-// create array to track camels and their positions on the board
-let camelArr = [
-    {color: "blue", position: 0},
-    {color: "green", position: 0},
-    {color: "orange", position: 0},
-    {color: "yellow", position: 0},
-    {color: "white", position: 0}
-]; // all start at position 0 by default but maybe should be white
+let SelectedCamel = null;
 // display game
 displayGame();
 
@@ -16,7 +7,12 @@ function displayGame() {
     displayCamelSelector();    
 }
 
+function clearDisplay() {
+    document.querySelector("#master").innerHTML = "";
+}
+
 function displayBoard() {
+    clearDisplay();
     const numPositions = 16; // the number of spaces in a lap
     let boardContainer = document.createElement("section");
     boardContainer.setAttribute("id", "boardContainer");
@@ -27,9 +23,18 @@ function displayBoard() {
         boardPos.setAttribute("class", "pos");
         boardPos.setAttribute("number", posNum);
         boardPos.innerText = "position: " + posNum;
-        for (const camel of camelArr) {
+        boardPos.addEventListener("click", (event) => {
+            for (const camel of GameState.camels) {
+                if (camel.color === SelectedCamel) {
+                    const clickedPos = event.srcElement.getAttribute("number");
+                    camel.position = Number(clickedPos);
+                    displayGame();
+                } 
+            }
+        });
+        for (const camel of GameState.camels) {
             if (camel.position === posNum) {
-                let camelIcon = document.createElement("p");
+                let camelIcon = document.createElement("p"); // this can be changed to an image or something
                 camelIcon.innerText = camel.color;
                 boardPos.appendChild(camelIcon);
             }
@@ -42,4 +47,23 @@ function displayCamelSelector() {
     let camelSelectorContainer = document.createElement("section");
     camelSelectorContainer.setAttribute("id", "camelSelectorContainer");
     document.querySelector("#master").appendChild(camelSelectorContainer);
+    for (const camel of GameState.camels) {
+        let camelSelector = document.createElement("section");
+        camelSelector.setAttribute("id", "camelSelector" + camel.color);
+        camelSelector.setAttribute("name", camel.color);
+        camelSelector.setAttribute("class", "camelSelectorUnselected");
+        camelSelector.addEventListener("click", (event) => {
+            //clear previous selected camel
+            for (const item of document.querySelector("#camelSelectorContainer").children) {
+                item.setAttribute("class", "camelSelectorUnselected");
+            }
+            //highlight new selected camel
+            document.querySelector("#"+event.srcElement.id).setAttribute("class", "camelSelectorSelected");
+            SelectedCamel = event.srcElement.getAttribute("name");
+        }); // this event trigger is extremely jank right now as you have to click in the right place
+        let camelIcon = document.createElement("p"); // this can be changed to an image or something
+        camelIcon.innerText = camel.color;
+        camelSelector.appendChild(camelIcon);
+        camelSelectorContainer.appendChild(camelSelector);
+    }
 }
