b8744c34f886b6ac2edf07bb7dd987602d779cd2
Rasengangstarr <55991814+Rasengangstarr@users.noreply.github.com>
2019-12-30 15:07:36 +0000

    calculate odds on round end

--- ui.js
+++ ui.js
@@ -112,6 +112,7 @@ function updateCamelSelector() {
             camel.hasMoved = false;
         }
         readyCamels = getReadyCamels();
+        CalculateOdds();
     }
     //get next camel index
     if (readyCamels.length === 1) {
